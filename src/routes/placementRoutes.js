0d1ae10e73c5982f6c7f705045bf6058085d34b5
krativeweb <krativewebdeveloper@gmail.com>
2026-01-02 15:28:28 +0530

    Update placementRoutes.js

--- src/routes/placementRoutes.js
+++ src/routes/placementRoutes.js
@@ -65,6 +65,29 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/:slug", async (req, res) => {
+  try {
+    const { slug } = req.params;
+
+    const seo = await Placement.findOne({
+      page_slug: slug,
+    }).lean();
+
+    if (!seo) {
+      return res.status(404).json(null);
+    }
+
+    // 🔥 IMPORTANT: return ONLY the object
+    res.status(200).json(seo);
+  } catch (err) {
+    res.status(500).json({
+      message: "Server error",
+      error: err.message,
+    });
+  }
+});
+
+
 /* ----------------------------------------------------
    UPDATE PAGE  — DELETE OLD IMAGE, SAVE NEW IMAGE
    AND REPLACE ENTIRE GALLERY
